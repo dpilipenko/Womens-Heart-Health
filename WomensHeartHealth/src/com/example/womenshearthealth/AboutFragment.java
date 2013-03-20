8fb3556d663756a13c491e247919d606178d2398
smith7018 <brandonsmith@192.168.0.107>
2013-03-20 18:13:02 -0400

    Test commit
    TESTTESTTESTTESTTEST

--- WomensHeartHealth/src/com/example/womenshearthealth/AboutFragment.java
+++ WomensHeartHealth/src/com/example/womenshearthealth/AboutFragment.java
@@ -11,7 +11,7 @@ public class AboutFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-
+		// Testing
 		// load in the fragment_about.xml file and display it on screen
 		return inflater.inflate(R.layout.fragment_about, container, false);
 		

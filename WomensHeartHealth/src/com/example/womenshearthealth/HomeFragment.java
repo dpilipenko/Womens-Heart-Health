fc6dad285dbe025214fbc0a7ca3641a51a6701a8
smith7018 <brandonsmith@192.168.0.107>
2013-03-20 20:45:29 -0400

    Added more eyecandy
    Just enjoy.

--- WomensHeartHealth/src/com/example/womenshearthealth/HomeFragment.java
+++ WomensHeartHealth/src/com/example/womenshearthealth/HomeFragment.java
@@ -1,8 +1,6 @@
 package com.example.womenshearthealth;
 
 
-import com.jjoe64.graphview.BarGraphView;
-import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.LineGraphView;
@@ -12,10 +10,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebView.FindListener;
-import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -26,15 +25,72 @@ public class HomeFragment extends Fragment {
 			Bundle savedInstanceState) {
 		
 		// load in the fragment_home.xml file and display it on screen
-		return inflater.inflate(R.layout.fragment_home, container, false);
-		
+		View homeFragment =  inflater.inflate(R.layout.fragment_home, container, false);
+		return homeFragment;
+	}
+	
+	@Override
+	public void onStart() {
+	super.onStart();
+	
+	LinearLayout graphCard = (LinearLayout) this.getActivity().findViewById(R.id.graph_card);
+	LinearLayout targetHRCard = (LinearLayout) this.getActivity().findViewById(R.id.target_hr_card);
+	LinearLayout totalsCard = (LinearLayout) this.getActivity().findViewById(R.id.totals_card);
+	
+	graphCard.setVisibility(8);
+	targetHRCard.setVisibility(8);
+	totalsCard.setVisibility(8);
+	
+	View graphCardDropshadow = (View) this.getActivity().findViewById(R.id.graphCardDropshadow);
+	View targetHRCardDropshadow = (View) this.getActivity().findViewById(R.id.targerHRCardDropshadow);
+	View totalsCardDropshadow = (View) this.getActivity().findViewById(R.id.totalsCardDropshadow);
+
+	graphCardDropshadow.setVisibility(8);
+	targetHRCardDropshadow.setVisibility(8);
+	totalsCardDropshadow.setVisibility(8);
+	
 	}
 	
 	@Override
 	public void onResume() {
 		super.onResume();
+		startAnimationPopOut(R.id.graph_card);
+		startAnimationPopOut(R.id.graphCardDropshadow);
+		startAnimationPopOut(R.id.target_hr_card);
+		startAnimationPopOut(R.id.targerHRCardDropshadow);
+		startAnimationPopOut(R.id.totals_card);
+		startAnimationPopOut(R.id.totalsCardDropshadow);
+		
 		populateUI();
 	}
+	
+	private void startAnimationPopOut(int id) {         
+	    View myLayout = (View) getActivity().findViewById(id);
+	    myLayout.setVisibility(0);
+	    Animation animation = AnimationUtils.loadAnimation(getActivity(),R.anim.slide_up_from_right);
+
+	    animation.setAnimationListener(new AnimationListener() {                  
+	        @Override
+	        public void onAnimationStart(Animation animation) {
+
+	        }
+
+	        @Override
+	        public void onAnimationRepeat(Animation animation) {
+
+	        }
+
+	        @Override
+	        public void onAnimationEnd(Animation animation) {
+
+	        }
+	    });
+
+	    myLayout.clearAnimation();
+	    myLayout.startAnimation(animation);
+
+	}
+
 
 	/**
 	 * Loads saved data and fills in the UI elements

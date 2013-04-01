8d09e0505b7cbe6d7a7a3eb868323976ee1e8b58
Zack Hal <hall.1502@gmail.com>
2013-04-01 18:32:30 -0400

    Adding Unit Testing Project

---
+++ WomensHeartHealthTest/src/com/example/womenshearthealth/test/CalculationsHelperTest.java
@@ -0,0 +1,173 @@
+package com.example.womenshearthealth.test;
+
+import junit.framework.TestCase;
+import com.example.womenshearthealth.*;
+
+public class CalculationsHelperTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testCalculateMaxHeartRate() {
+		int age, expectedHR, calculatedHR;
+		
+		age = 100;
+		expectedHR = 118;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_MAX);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 80;
+		expectedHR = 135;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_MAX);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 60;
+		expectedHR = 153;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_MAX);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 40;
+		expectedHR = 170;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_MAX);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 20;
+		expectedHR = 188;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_MAX);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 0;
+		expectedHR = 206;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_MAX);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+	}
+	
+	public void testCalculate85HeartRate() {
+		int age, expectedHR, calculatedHR;
+		
+		age = 100;
+		expectedHR = 100;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_85);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 80;
+		expectedHR = 115;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_85);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 60;
+		expectedHR = 130;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_85);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 40;
+		expectedHR = 145;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_85);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 20;
+		expectedHR = 160;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_85);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 0;
+		expectedHR = 175;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_85);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+	}
+	
+	public void testCalculate50HeartRate() {
+		int age, expectedHR, calculatedHR;
+		
+		age = 100;
+		expectedHR = 59;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_50);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 80;
+		expectedHR = 67;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_50);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 60;
+		expectedHR = 76;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_50);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 40;
+		expectedHR = 85;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_50);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 20;
+		expectedHR = 94;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_50);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+		
+		age = 0;
+		expectedHR = 103;
+		calculatedHR = CalculationsHelper.getHeartRateFromAge(age, CalculationsHelper.HRTARGET_50);
+		assertTrue("Age: " + age + "; Expected HR: " + expectedHR + "; Calculated HR: " + calculatedHR + "."
+				, calculatedHR == expectedHR);
+	}
+	
+	public void testCalculateCaloriesFromMet()
+	{
+		double ACCEPTABLE_PERCENT_ERROR = 0.05;
+		
+		int weight;
+		double metHours, calculatedCals, expectedCals;
+		
+		weight = 220;
+		metHours = 3;
+		expectedCals = 299;
+		calculatedCals = CalculationsHelper.getCaloriesFromMet(weight, metHours);
+		assertTrue("Calculated Calories (" + calculatedCals + ") outside acceptable error of " +
+					"Expected Calories (" + expectedCals + "). Acceptable Error: " + ACCEPTABLE_PERCENT_ERROR,
+				(Math.abs(expectedCals - calculatedCals) / expectedCals) < ACCEPTABLE_PERCENT_ERROR);
+		
+		weight = 100;
+		metHours = 13.5;
+		expectedCals = 612;
+		calculatedCals = CalculationsHelper.getCaloriesFromMet(weight, metHours);
+		assertTrue("Calculated Calories (" + calculatedCals + ") outside acceptable error of " +
+					"Expected Calories (" + expectedCals + "). Acceptable Error: " + ACCEPTABLE_PERCENT_ERROR,
+				(Math.abs(expectedCals - calculatedCals) / expectedCals) < ACCEPTABLE_PERCENT_ERROR);
+		
+		weight = 135;
+		metHours = 3.3;
+		expectedCals = 202;
+		calculatedCals = CalculationsHelper.getCaloriesFromMet(weight, metHours);
+		assertTrue("Calculated Calories (" + calculatedCals + ") outside acceptable error of " +
+					"Expected Calories (" + expectedCals + "). Acceptable Error: " + ACCEPTABLE_PERCENT_ERROR,
+				(Math.abs(expectedCals - calculatedCals) / expectedCals) < ACCEPTABLE_PERCENT_ERROR);
+		
+		weight = 160;
+		metHours = 6.5;
+		expectedCals = 472;
+		calculatedCals = CalculationsHelper.getCaloriesFromMet(weight, metHours);
+		assertTrue("Calculated Calories (" + calculatedCals + ") outside acceptable error of " +
+					"Expected Calories (" + expectedCals + "). Acceptable Error: " + ACCEPTABLE_PERCENT_ERROR,
+				(Math.abs(expectedCals - calculatedCals) / expectedCals) < ACCEPTABLE_PERCENT_ERROR);
+		
+	}
+
+}
